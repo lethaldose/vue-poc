1182e85e3b8e8eaa2b5244a757fec97d6b6f9c21
Atul Verma <atul.verma@indebted.co>
2020-02-10 16:46:27 +1100

    Adding vuetify material framework

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -5,25 +5,54 @@ export default {
 </script>
 
 <template>
-  <div :class="$style.container">
-    <div>Header</div>
-    <Transition name="fade" mode="out-in">
+  <v-app :class="$style.container">
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Templates</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
       <RouterView />
-    </Transition>
-  </div>
+    </v-content>
+
+    <v-footer color="primary" app>
+      <span class="white--text">&copy; 2020</span>
+    </v-footer>
+  </v-app>
 </template>
+
+<script>
+export default {
+  name: "BaseLayout",
+  data: () => ({
+    drawer: null,
+  }),
+}
+</script>
 
 <style lang="stylus" module>
 .container {
   margin: 0 auto;
-}
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s;
-}
-
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
 }
 </style>
